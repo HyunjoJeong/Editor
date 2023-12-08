a8d59ec1d0609ffcb9bd4d94982cb9528a3c8938
hj-Jeong01 <hj.jeong@fanmaum.com>
2023-12-08 09:01:13 +0900

    git ignore 수정

--- dist/types/Editor.d.ts
+++
@@ -1,8 +0,0 @@
-import React, { CSSProperties } from "react";
-type EditorProps = {
-    initialContent?: string;
-    toolBar?: string[][];
-    wrapperStyle?: CSSProperties;
-};
-export default function Editor({ initialContent, toolBar, wrapperStyle, }: EditorProps): React.JSX.Element;
-export {};
